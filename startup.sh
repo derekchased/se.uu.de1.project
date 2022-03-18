50b81293ee599fda57a7319bf7c0523159c397d1
Derek Yadgaroff <derek@ibelievedigital.com>
2022-03-18 10:54:01 +0100

    update script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# a) Copy this file into ~
+# b) Change permissions of this file chmod u+x startup.sh
+# c) Run this script ./startup.sh
+
 # 1. Update VM
 
 sudo apt update;
@@ -37,68 +41,5 @@ cd reddit;
 mkdir volume
 
 # 6. Mount volume - Do this manually using correct path to volume
+# sudo lsblk -f
 # sudo mount <PATH TO VOLUME /dev/vdb> ./volume
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## Copy docker compose file from VM volume snapshot into reddit app directory
-
-# cp <VOLUME PATH/docker-compose.yml> reddit
-
-## Make data directory docker volume bind
-
-mkdir data;
-
-## Copy reddit files from VM volume snapshot into reddit directory data directory docker volume bind
-
-# cp <VOLUME PATH/*> reddit/data
-
-
-## A) Start Swarm Manager
-
-# docker swarm init
-
-# docker swarm init --advertise-addr <IP ADDRESS OF MANAGER NODE>
-# docker swarm init --advertise-addr 130.238.28.102
-
-## Check docker
-# docker info
-# docker node ls
-
-## B) Add Swarm Nodes, run this command on each VM worker
-
-# this is from the output when we create the manager
-# docker swarm join --token <token>
-
-# you can also get this link if you lose it running 
-# docker swarm join-token worker
-
-## Label docker nodes
-
-# docker node update --addlabel <container id of master node> "sparkmaster"
-# docker node update --addlabel <container id of all worker nodes> "sparkworker"
-
-## Deploy stack to swarm 
-
-# docker stack deploy -c 
